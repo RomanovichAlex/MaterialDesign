ca5c600552601fc6ecb3bb8b48d351630471cb0d
RomanovichAlex <romanowitch.alex@yandex.ru>
2022-02-28 19:45:24 +0300

    Урок 2. Создал три темы, в settingFragment, добавил возможность их менять. Добавил шрифт.

--- app/src/main/java/by/romanovich/materialdesign/view/MainActivity.kt
+++ app/src/main/java/by/romanovich/materialdesign/view/MainActivity.kt
@@ -1,22 +1,35 @@
 package by.romanovich.materialdesign.view
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import by.romanovich.materialdesign.R
 import by.romanovich.materialdesign.view.main.MainFragment
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MyBlackBlue)
-        setContentView(R.layout.activity_main)
 
-
-        if(savedInstanceState==null){
-            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commit()
+        when(getPreferences(Activity.MODE_PRIVATE).getString("settingTheme","")){
+            "chipDay"->{
+                setTheme(R.style.MyDarkTheme)
+            }
+            "chipMars"->{
+                setTheme(R.style.MensTheme)
+            }
+            "chipMoon"->{
+                setTheme(R.style.GirlsTheme)
+            }
         }
 
 
-        //recreate() перезагружает активити и можно выбрать нужную тему
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commit()
+        }
     }
 }
+        //recreate() перезагружает активити и можно выбрать нужную тему
--- app/src/main/java/by/romanovich/materialdesign/view/settings/SettingsFragment.kt
+++ app/src/main/java/by/romanovich/materialdesign/view/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package by.romanovich.materialdesign.view.settings
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat.recreate
 import androidx.fragment.app.Fragment
 import by.romanovich.materialdesign.R
 import by.romanovich.materialdesign.databinding.FragmentSettingsBinding
@@ -28,14 +30,16 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        initChip()
+        clickChip()
+
         binding.chipGroup.setOnCheckedChangeListener { group, checkedId ->
             //у чип гроуп находим по позиции, которую выбрали
             binding.chipGroup.findViewById<Chip>(checkedId)?.let{ it->
@@ -47,6 +51,9 @@ class SettingsFragment : Fragment() {
         binding.chipEntry.setOnCloseIconClickListener {
             Toast.makeText(requireContext(), "chipEntry close", Toast.LENGTH_SHORT).show()
         }
+
+
+
 
 
 //верни мне вкладку по индексу, задаем текст и иконки
@@ -69,8 +76,51 @@ class SettingsFragment : Fragment() {
 
 
 
+    private fun initChip() {
+        when(requireActivity().getPreferences(Activity.MODE_PRIVATE).getString("settingTheme","")){
+            "chipDay"->{
+                binding.chipDark.isChecked = true
+            }
+            "chipMars"->{
+                binding.chipMens.isChecked = true
+            }
+            "chipMoon"->{
+                binding.chipGirls.isChecked = true
+            }
+        }
+    }
+
+    private fun clickChip() {
+        binding.chipGroup1.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.chipDark->{
+                    requireActivity().getPreferences(Activity.MODE_PRIVATE).edit().putString("settingTheme","chipDay").apply()
+                    requireActivity().setTheme(R.style.MyDarkTheme)
+                    recreate(requireActivity())
+                }
+                R.id.chipMens->{
+                    requireActivity().getPreferences(Activity.MODE_PRIVATE).edit().putString("settingTheme","chipMars").apply()
+                    requireActivity().setTheme(R.style.MensTheme)
+                    recreate(requireActivity())
+                }
+                R.id.chipGirls->{
+                    requireActivity().getPreferences(Activity.MODE_PRIVATE).edit().putString("settingTheme","chipMoon").apply()
+                    requireActivity().setTheme(R.style.GirlsTheme)
+                    recreate(requireActivity())
+                }
+            }
+        }
+    }
+
+
+
     companion object {
         @JvmStatic
         fun newInstance() = SettingsFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
