c1d92233071744825a42c1a0e71a35fce024a80b
RomanovichAlex <romanowitch.alex@yandex.ru>
2022-03-21 16:36:01 +0300

    Урок 5. Познакомились ObjectAnimator + View.animate()

--- app/src/main/java/by/romanovich/materialdesign/view/animations/AnimationsActivity.kt
+++ app/src/main/java/by/romanovich/materialdesign/view/animations/AnimationsActivity.kt
@@ -1,57 +1,108 @@
 package by.romanovich.materialdesign.view.animations
 
-import android.graphics.ImageDecoder
-import android.graphics.Rect
-import android.hardware.biometrics.BiometricManager
+
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.*
-import by.romanovich.materialdesign.R
 import by.romanovich.materialdesign.databinding.ActivityAnimationsBinding
-import kotlin.random.Random
+
 
 
 class AnimationsActivity : AppCompatActivity() {
 
     private var flag = false
+    private var duration = 1000L
     lateinit var binding: ActivityAnimationsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val titles: MutableList<String> = ArrayList()
-        repeat(5){
-            titles.add("Item${it}")
-        }
+        binding.transparentBackground.alpha = 0f
+        binding.optionOneContainer.alpha = 0f
+        binding.optionOneContainer.isClickable= false
+        binding.optionTwoContainer.alpha = 0f
+        binding.optionTwoContainer.isClickable= false
 
-        binding.button.setOnClickListener {
-            flag = !flag
-            //перемешиваем
-            titles.shuffle()
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,ChangeBounds())
-            binding.transitionsContainer.removeAllViews()
+        binding.fab.setOnClickListener {
+            flag = ! flag
+            if (flag) {
+                //крутим +
+                //ObjectAnimator.ofFloat(binding.plusImageview,"rotation",0f,405f).start()
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 0f, 405f).
+                setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, 0f, -130f).
+                setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, 0f, -260f).
+                setDuration(duration).start()
 
-            titles.forEach{
-                binding.transitionsContainer.addView(TextView(this).apply {
-                    text = it
-                    textSize = 20f
-                    //присваеваем удаленным элементам те же псевдонимы
-                    transitionName = it
-                    gravity = Gravity.CENTER_HORIZONTAL
-                })
+                //анимируем выезджание
+                binding.optionTwoContainer.animate().alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object: AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.optionTwoContainer.isClickable= true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+                //анимируем выезджание
+                binding.optionOneContainer.animate().alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object: AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.optionOneContainer.isClickable= true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                //анимируем затемнение фона
+                binding.transparentBackground.animate().alpha(0.8f)
+                    .setDuration(duration)
+                    .setListener(object: AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.transparentBackground.isClickable= true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+            }else{
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 405f, 0f).
+                setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -130f, 0f).
+                setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -260f, 0f).
+                setDuration(duration).start()
+
+                binding.optionTwoContainer.animate().alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object: AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.optionTwoContainer.isClickable= false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+                binding.optionOneContainer.animate().alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object: AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.optionOneContainer.isClickable= false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.transparentBackground.animate().alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object: AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.transparentBackground.isClickable= false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
             }
-
         }
-
-
     }
 }
