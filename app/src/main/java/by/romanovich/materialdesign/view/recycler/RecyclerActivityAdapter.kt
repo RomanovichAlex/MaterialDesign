b379513f7f9a4341881f2d3912d4d6c6f648cdf7
RomanovichAlex <romanowitch.alex@yandex.ru>
2022-03-23 20:04:05 +0300

    Урок 6. Начало. Переделали базовый ViewHolder

--- app/src/main/java/by/romanovich/materialdesign/view/recycler/RecyclerActivityAdapter.kt
+++ app/src/main/java/by/romanovich/materialdesign/view/recycler/RecyclerActivityAdapter.kt
@@ -6,45 +6,53 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 import by.romanovich.materialdesign.databinding.ActivityRecyclerItemEarthBinding
+import by.romanovich.materialdesign.databinding.ActivityRecyclerItemHeaderBinding
 import by.romanovich.materialdesign.databinding.ActivityRecyclerItemMarsBinding
 
 class RecyclerActivityAdapter(private val onListItemClickListener:OnListItemClickListener,
                               private val data: List<Data>):
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<RecyclerActivityAdapter.BaseViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
         return data[position].type
     }
     //создаем
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         if (viewType == TYPE_EARTH) {
             val itemBinding: ActivityRecyclerItemEarthBinding =
                 ActivityRecyclerItemEarthBinding.inflate(
                     LayoutInflater.from(parent.context), parent, false)
             return EarthViewHolder(itemBinding.root)
-        } else {
+        }else if (viewType == TYPE_MARS) {
             val itemBinding: ActivityRecyclerItemMarsBinding =
                 ActivityRecyclerItemMarsBinding.inflate(
                     LayoutInflater.from(parent.context), parent, false)
             return MarsViewHolder(itemBinding.root)
+            
+        } else {
+            val itemBinding: ActivityRecyclerItemHeaderBinding =
+                ActivityRecyclerItemHeaderBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false)
+            return HeaderViewHolder(itemBinding.root)
 
         }
     }
 
     //связываем созданый ViewHolder с данными
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (getItemViewType(position) == TYPE_EARTH){
-            (holder as EarthViewHolder).bind(data[position])
-        }else{
-            (holder as MarsViewHolder).bind(data[position])
-        }
-        //holder.bind(data[position])
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+
+        holder.bind(data[position])
     }
 
     override fun getItemCount() = data.size
 
-    inner class EarthViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(data: Data) {
+    abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        abstract fun bind(data: Data)
+
+    }
+
+    inner class EarthViewHolder(view: View) : BaseViewHolder(view) {
+        override fun bind(data: Data) {
             ActivityRecyclerItemEarthBinding.bind(itemView).apply {
                 earthImageView.setOnClickListener {
                     onListItemClickListener.onListItem(data)
@@ -53,10 +61,20 @@ class RecyclerActivityAdapter(private val onListItemClickListener:OnListItemClic
         }
     }
 
-    inner class MarsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(data: Data) {
+    inner class MarsViewHolder(view: View) : BaseViewHolder(view) {
+        override fun bind(data: Data) {
             ActivityRecyclerItemMarsBinding.bind(itemView).apply {
                 marsImageView.setOnClickListener {
+                    onListItemClickListener.onListItem(data)
+                }
+            }
+        }
+    }
+
+    inner class HeaderViewHolder(view: View) : BaseViewHolder(view) {
+        override fun bind(data: Data) {
+            ActivityRecyclerItemHeaderBinding.bind(itemView).apply {
+                header.setOnClickListener {
                     onListItemClickListener.onListItem(data)
                 }
             }
